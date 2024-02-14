89af95cc82c9166207f15dc6d224df77542d5af7
ian-summers <ian.robert.summers@gmail.com>
2024-02-13 21:18:17 -0700

    added amazon heatmap js file

---
+++ amazon-heatmap.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function(){
+    fetch('database_data.json')
+    .then(response => response.json())
+    .then(data => {
+        var amazonData = data.amazon;
+        
+        var trace = {
+            type: 'scatter',
+            mode: 'markers',
+            x: amazonData.map(d => d.date),
+            y: amazonData.map(d => d.close),
+            marker: {
+                size: amazonData.map(d => Math.sqrt(d.volume) / 250), // Adjust marker size for better visibility
+                color: amazonData.map(d => d.volume),
+                colorscale: 'Cividis',
+                showscale: true // Show color scale to interpret volume
+            }
+        };
+
+        var layout = {
+            title: 'Amazon (AMZN) Closing Day Value vs Volume Over Time',
+            plot_bgcolor: 'ivory',
+            paper_bgcolor: 'ivory',
+            xaxis: { title: 'Date' },
+            yaxis: { 
+                title: 'Close Value' },
+                range: [0, 'auto'],
+                rangemode: 'nonnegative' 
+        };
+
+        Plotly.newPlot('myDiv', [trace], layout);
+    })
+    .catch(error => console.error('Error loading the JSON data:', error));
+});
